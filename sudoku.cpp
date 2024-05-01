14e3cca90ad60b585ac2814e83445cb9fc89a2f3
codycherrington <86078360+codycherrington@users.noreply.github.com>
2024-05-01 16:26:38 -0500

    sudoku.cpp

--- sudoku.cpp
+++ sudoku.cpp
@@ -2,9 +2,11 @@
 #include <ctime>
 
 using namespace std;
+using namespace std::chrono;
 
 // Function to check if placing 'n' at position (x, y) in the row is valid
 bool checkRow(int n, int board[][9], int x, int y) {
+
     // Iterate backwards from the current position (x, y) to check the row
     for (int i = x - 1; i >= 0; i--) {
         // If 'n' is found in the same row, return false
@@ -18,6 +20,7 @@ bool checkRow(int n, int board[][9], int x, int y) {
 
 // Function to check if placing 'n' at position (x, y) in the column is valid
 bool checkCol(int n, int board[][9], int x, int y) {
+
     // Iterate forwards from the current position (x, y) to check the column
     for (int i = y + 1; i < 9; i++) {
         // If 'n' is found in the same column, return false
@@ -31,6 +34,7 @@ bool checkCol(int n, int board[][9], int x, int y) {
 
 // Function to check if placing 'n' at position (x, y) in the 3x3 box is valid
 bool checkBox(int n, int board[][9], int x, int y) {
+
     // Calculate the starting position of the 3x3 box based on (x, y)
     int startX = (x / 3) * 3;
     int startY = (y / 3) * 3;
@@ -47,8 +51,24 @@ bool checkBox(int n, int board[][9], int x, int y) {
     return true;
 }
 
+// Function to check if the user has won the game
+bool checkWin(int solvedBoard[][9], int userBoard[][9]) {
+
+    for (int y = 0; y < 9; ++y) { 
+        for (int x = 0; x < 9; ++x) {
+            // If any cell in the user board doesn't match the corresponding cell in the solved board, return false
+            if (userBoard[x][y] != solvedBoard[x][y]) {
+                return false; 
+            }
+        }
+    }
+    // If all cells match, return true
+    return true; 
+}
+
 // Function to randomly fill the Sudoku board
 void fillBoard(int board[][9]) {
+
     // Seed the random number generator
     srand(time(NULL));
     int y = 8;
@@ -82,6 +102,79 @@ void fillBoard(int board[][9]) {
         }
         // Move to the next row
         y--;
+    }
+}
+
+// Function to fill the user board based on the difficulty level
+void fillUserBoard(int solvedBoard[][9], int userBoard[][9]) {
+
+    // Copy the solved board to the user board
+    for (int y = 8; y >= 0; --y) {
+        for (int x = 0; x < 9; x++) {
+            int n = solvedBoard[x][y];
+            userBoard[x][y] = n;
+        }
+    }
+
+    // Prompt the user to select the difficulty level
+    int difficulty = 0;
+    while (true) {
+
+        //Main menu header
+        cout << "            Main Menu      " << endl;
+        cout << "     -----------------------" << endl;
+        cout << "      Select your difficulty" << endl << endl;
+        cout << "          1 for Easy" << endl; 
+        cout << "          2 for Medium" << endl; 
+        cout << "          3 for Hard" << endl;
+        cout << "          0 to exit." << endl << endl;
+        cout << "         Difficulty: "; 
+        cin >> difficulty;
+
+        // Validate the user input
+        if (cin.fail()) {
+            cout << endl << "       ***Invalid input***" << endl << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            continue;
+        }
+
+        if (difficulty == 0) {
+            cout << endl << "       Thank you for playing!" << endl;
+            cout << "    Written by Cody Cherrington." << endl << "               2024" << endl << endl << endl;
+            exit(0);
+        }
+
+        if (difficulty != 1 && difficulty != 2 && difficulty != 3 && difficulty != 1998) {
+            cout << endl << "       ***Invalid input***" << endl << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            continue;
+        }
+        cout << endl << "     -----------------------" << endl;
+        break;
+    }
+
+    // Determine the number of zeros to be added based on the difficulty level
+    int numZeros;
+    srand(time(NULL));
+    if (difficulty == 1) {
+        numZeros = rand() % 9 + 27;
+    }
+    if (difficulty == 2) {
+        numZeros = rand() % 13 + 35;
+    }
+    if (difficulty == 3) {
+        numZeros = rand() % 18 + 47;
+    }
+    if (difficulty == 1998) {
+        numZeros = 1;
+    }
+    // Randomly replace cells with zeros to create the puzzle
+    for (int i = 0; i < numZeros; ++i) {
+        int x = rand() % 9;
+        int y = rand() % 9;
+        userBoard[x][y] = 0; 
     }
 }
 
@@ -141,22 +234,10 @@ void displayBoard(int board[][9], int& lives) {
     cout << "            0 to exit" << endl;
 }
 
-// Function to check if the user has won the game
-bool checkWin(int solvedBoard[][9], int userBoard[][9]) {
-    for (int y = 0; y < 9; ++y) { 
-        for (int x = 0; x < 9; ++x) {
-            // If any cell in the user board doesn't match the corresponding cell in the solved board, return false
-            if (userBoard[x][y] != solvedBoard[x][y]) {
-                return false; 
-            }
-        }
-    }
-    // If all cells match, return true
-    return true; 
-}
-
 // Function for user input and gameplay
 void userInput(int userBoard[][9], int solvedBoard[][9]) {
+
+    //Initialize values
     int x = 0;
     int y = 0;
     int n = 0;
@@ -164,8 +245,13 @@ void userInput(int userBoard[][9], int solvedBoard[][9]) {
 
     // Display the initial state of the Sudoku board
     displayBoard(userBoard, lives);
+
+    // Start time
+    time_t start = time(NULL);
     
     while (true) {
+
+        //Get x coordinate
         cout << endl << "Enter x value (1 through 9): ";
         cin >> x;
 
@@ -188,6 +274,7 @@ void userInput(int userBoard[][9], int solvedBoard[][9]) {
             break;
         }
 
+        //Get y coordinate
         cout << "Enter y value (1 through 9): ";
         cin >> y;
 
@@ -212,12 +299,13 @@ void userInput(int userBoard[][9], int solvedBoard[][9]) {
 
         // Check if the selected cell is already filled
         if (userBoard[x - 1][y - 1] > 0) {
-            cout << endl << "     ***Invalid entry***" << endl;
+            cout << endl << "   ***Cell already filled!***" << endl;
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
             continue;
         }
 
+        //Get number to enter in cell (x, y)
         cout << "Enter number (1 through 9): ";
         cin >> n;
         cout << endl;
@@ -268,77 +356,29 @@ void userInput(int userBoard[][9], int solvedBoard[][9]) {
 
         // Check if the user has won the game
         if (checkWin(solvedBoard, userBoard) == true) {
-            cout << endl << "        Congratulations!!!" << endl << "     You solved the puzzle!!!" << endl << endl;
+
+            // End time
+            time_t end = time(NULL);
+            int duration = difftime(end, start);
+
+            int minutes = duration / 60;
+            int seconds = duration % 60;
+
+            cout << endl << "       Congratulations!!!" << endl << endl
+                 << "     You solved the puzzle in " << endl << "     " << minutes << " minutes and "
+                 << seconds % 60 << " seconds." << endl << endl;
+
+            if (lives == 3) {
+                cout << "    You had " << lives << " lives remaining." << endl << endl;
+            }
+            if (lives == 2) {
+                cout << "    You had " << lives << " lives remaining." << endl << endl;
+            }
+            if (lives == 1) {
+                cout << "    You had " << lives << " life remaining." << endl << endl;
+            }
             break;
         }
-    }
-}
-
-// Function to fill the user board based on the difficulty level
-void fillUserBoard(int solvedBoard[][9], int userBoard[][9]) {
-    // Copy the solved board to the user board
-    for (int y = 8; y >= 0; --y) {
-        for (int x = 0; x < 9; x++) {
-            int n = solvedBoard[x][y];
-            userBoard[x][y] = n;
-        }
-    }
-
-    // Prompt the user to select the difficulty level
-    int difficulty = 0;
-    while (true) {
-        cout << "      Select your difficulty" << endl << endl;
-        cout << "          1 for Easy" << endl; 
-        cout << "          2 for Medium" << endl; 
-        cout << "          3 for Hard" << endl;
-        cout << "          0 to exit." << endl << endl;
-        cout << "         Difficulty: "; 
-        cin >> difficulty;
-
-        // Validate the user input
-        if (cin.fail()) {
-            cout << endl << "       ***Invalid input***" << endl << endl;
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            continue;
-        }
-
-        if (difficulty == 0) {
-            cout << endl << "       Thank you for playing!" << endl;
-            cout << "    Written by Cody Cherrington." << endl << "               2024" << endl << endl << endl;
-            exit(0);
-        }
-
-        if (difficulty != 1 && difficulty != 2 && difficulty != 3 && difficulty != 1998) {
-            cout << endl << "       ***Invalid input***" << endl << endl;
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            continue;
-        }
-        cout << endl << "     -----------------------" << endl;
-        break;
-    }
-
-    // Determine the number of zeros to be added based on the difficulty level
-    int numZeros;
-    srand(time(NULL));
-    if (difficulty == 1) {
-        numZeros = rand() % 9 + 27;
-    }
-    if (difficulty == 2) {
-        numZeros = rand() % 13 + 35;
-    }
-    if (difficulty == 3) {
-        numZeros = rand() % 18 + 47;
-    }
-    if (difficulty == 1998) {
-        numZeros = 1;
-    }
-    // Randomly replace cells with zeros to create the puzzle
-    for (int i = 0; i < numZeros; ++i) {
-        int x = rand() % 9;
-        int y = rand() % 9;
-        userBoard[x][y] = 0; 
     }
 }
 
@@ -353,7 +393,7 @@ int main() {
         // Initialize the solved board and user board
         int solvedBoard[9][9] = {0};
         int userBoard[9][9] = {0};
-        //Initialize choice to continue the game or not
+        //Initialize player choice to continue the game or not
         char choice = ' ';
 
         // Fill the solved board with random values
